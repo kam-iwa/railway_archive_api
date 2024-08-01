f1876d14802f21c7140f74a04cc27edae3313db1
Kamil Iwaniuk <iwaniukkamil@gmail.com>
2024-08-01 02:13:26 +0200

    stops routings done

--- app/blueprints/core/routings_stops.py
+++ app/blueprints/core/routings_stops.py
@@ -1,3 +1,4 @@
+from flasgger import swag_from
 from flask import jsonify, request
 
 from blueprints.core import stop_mod
@@ -5,12 +6,36 @@ from models.core.models_stops import Stop
 
 
 @stop_mod.route('/stops', methods=['GET'])
+@swag_from('docs/stops.get.yml')
 def stops_get():
     stops = Stop.select()
-    return jsonify([{"id": stop.id} for stop in stops])
+    return jsonify([{
+        "id": stop.id,
+        "route": stop.route.id,
+        "station": stop.station.id,
+        "arrival_time": str(stop.arrival_time),
+        "departure_time": str(stop.departure_time)
+    } for stop in stops])
+
+
+@stop_mod.route('/stops/<int:stop_id>', methods=['GET'])
+@swag_from('docs/stops.stop_id.get.yml')
+def stops_stop_id_get(stop_id: int):
+    stop = Stop.get_or_none(Stop.id == stop_id)
+    if stop is None:
+        return jsonify({'error': 'Stop not found.'})
+
+    return jsonify({
+        "id": stop.id,
+        "route": stop.route.id,
+        "station": stop.station.id,
+        "arrival_time": str(stop.arrival_time),
+        "departure_time": str(stop.departure_time)
+    })
 
 
 @stop_mod.route('/stops', methods=['POST'])
+@swag_from('docs/stops.post.yml')
 def stops_create():
     data = request.get_json()["data"]
     stop = Stop.create(**data)
@@ -19,22 +44,27 @@ def stops_create():
 
 
 @stop_mod.route('/stops/<int:stop_id>', methods=["PUT"])
+@swag_from('docs/stops.stop_id.put.yml')
 def stop_update(stop_id: int):
-    stop = Stop.get_or_none(id == stop_id)
+    stop = Stop.get_or_none(Stop.id == stop_id)
     if stop is None:
         return jsonify({'error': 'Stop not found.'})
 
     data = request.get_json()["data"]
-    stop = Stop.update(**data)
+    query = Stop.update(**data).where(Stop.id == stop_id)
+    query.execute()
 
     return jsonify({}), 200
 
 
 @stop_mod.route('/stops/<int:stop_id>', methods=["DELETE"])
+@swag_from('docs/stops.stop_id.delete.yml')
 def stop_delete(stop_id: int):
-    stop = Stop.get_or_none(id == stop_id)
+    stop = Stop.get_or_none(Stop.id == stop_id)
     if stop is None:
         return jsonify({'error': 'stop not found.'})
 
-    Stop.delete().where(stop.id == stop_id)
+    query = Stop.delete().where(Stop.id == stop_id)
+    query.execute()
+
     return jsonify({}), 204
